3f5bb30fa070ec67fc98a35945d78543d33ea982
piyongze <piyongze@126.com>
2016-01-26 16:25:42 +0800

    一批0防伪URL错误，因而0开头的防伪链接统一跳至稽查

--- src/com/tzit/servet/WsShUrl.java
+++ src/com/tzit/servet/WsShUrl.java
@@ -12,45 +12,51 @@ import javax.servlet.http.HttpServletResponse;
  * »ü²éÊ¹ÓÃq
  */
 public class WsShUrl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public WsShUrl() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String p=request.getParameter("p");
-		String q=request.getParameter("q");
-		if(p!=null){
-			response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+p);
-		}
-		if(q!=null){
-			response.sendRedirect("http://www.tzscm.com/weishi/logicstics/scan?qrcode="+q);
-		}
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String p=request.getParameter("p");
+        String q=request.getParameter("q");
+        if(p!=null){
+            response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+p);
+        }
+        if(q!=null){
+            if(q.substring(0, 1).equals("0")){
+                response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+q);
+            }else{
+                response.sendRedirect("http://www.tzscm.com/weishi/logicstics/scan?qrcode="+q);
+            }
+            
+        }
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String p=request.getParameter("p");
-		String q=request.getParameter("q");
-		if(p!=null){
-			response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+p);
-		}
-		if(q!=null){
-			response.sendRedirect("http://www.tzscm.com/weishi/logicstics/scan?qrcode="+q);
-		}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 
-	}
+throws ServletException, IOException {
+        String p=request.getParameter("p");
+        String q=request.getParameter("q");
+        if(p!=null){
+            response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+p);
+        }
+        if(q!=null){
+            if(q.substring(0, 1).equals("0")){
+                response.sendRedirect("http://www.tzscm.com/weishi/antifake/scan?qrcode="+q);
+            }else{
+                response.sendRedirect("http://www.tzscm.com/weishi/logicstics/scan?qrcode="+q);
+            }
+            
+        }
 
+    }
+    
 }
